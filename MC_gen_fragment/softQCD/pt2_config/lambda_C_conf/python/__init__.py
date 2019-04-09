f73a1cb9a4baabae963e9e8ae45eb33dea5527d1
ruixiao491 <ruixiao491@yahoo.com>
2019-04-09 00:46:59 -0400

    MC_soft_genfiles

---
+++ MC_gen_fragment/softQCD/pt2_config/lambda_C_conf/python/__init__.py
@@ -0,0 +1,3 @@
+#Automatically created by SCRAM
+import os
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/pt2_config/lambda_C_conf/',1)[0])+'/cfipython/slc6_amd64_gcc630/pt2_config/lambda_C_conf')
